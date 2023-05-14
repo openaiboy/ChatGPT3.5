dec95da87f9020f3221a35cd870cb65533a59f80
dingfeng169 <64767075+dingfeng169@users.noreply.github.com>
2023-05-15 00:12:10 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -2,7 +2,7 @@ export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://thoughts.teambition.com/share/6460f47ec98de800422c4d5e#title=OpenAI学习笔记`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `{REPO_URL}`;
+export const UPDATE_URL = `https://thoughts.teambition.com/share/6460f47ec98de800422c4d5e#title=OpenAI学习笔记`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
