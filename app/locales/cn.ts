345b03fe743ea30221e14d128ca0796d9bc21253
dingfeng169 <64767075+dingfeng169@users.noreply.github.com>
2023-05-18 14:31:29 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -87,12 +87,12 @@ const cn = {
     },
 
     Update: {
-      Version: (x: string) => `当前版本：${x}`,
+      Version: (x: string) => `ChatGPT中文版`,
       IsLatest: "已是最新版本",
       CheckUpdate: "检查更新",
       IsChecking: "正在检查更新...",
-      FoundUpdate: (x: string) => `发现新版本：${x}`,
-      GoToUpdate: "前往更新",
+      FoundUpdate: (x: string) => `OpenAI & ChatGPT独享号`,
+      GoToUpdate: "点击订购",
     },
     SendKey: "发送键",
     Theme: "主题",
